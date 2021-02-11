1f0dff3b6ad0da833d2320dceca7be960419f056
dango17 <danieloldham75@gmail.com>
2021-02-11 23:08:19 +0000

    Pause & Main menu
    Pause & Main menu (Fix needed)

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuScreen;
+
+    public void PausedGame()
+    {
+        if(GameIsPaused)
+        {
+            Resume();
+        } 
+        else
+        {
+            Pause();
+        }
+    } 
+
+    public void Resume()
+    {
+        PauseMenuScreen.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    } 
+
+    void Pause()
+    {
+        PauseMenuScreen.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true; 
+    } 
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitApplication()
+    {
+        Debug.Log("Qutting game...");
+        Application.Quit();
+    }
+}
