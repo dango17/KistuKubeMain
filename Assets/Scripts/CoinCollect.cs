fc9be6027e8769232be3cd510cf0e9bdf591a271
dango17 <danieloldham75@gmail.com>
2021-02-22 19:20:09 +0000

    CollectionManager
    CollectionManager starting point

---
+++ Assets/Scripts/CoinCollect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+    public int coinAmount = 1;
+    
+    public void OnTriggerEnter(Collider col)
+    {
+        CollectManager.instance.UpdateCoinsUI(coinAmount);
+        CollectManager.instance.currentCoins += coinAmount;
+        Destroy(gameObject);      
+    }
+}
---
+++ Assets/Scripts/CollectManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class CollectManager : MonoBehaviour
+{
+    public static CollectManager instance; 
+    public int currentCoins;
+    public Text coinValueTxt; 
+
+    private void Awake()
+    {
+        instance = this;    
+    }
+ 
+    void Start()
+    {
+        
+        coinValueTxt.text = currentCoins.ToString() + "/3";
+    } 
+
+    public void UpdateCoinsUI(int coinAmount)
+    {
+        currentCoins += coinAmount; 
+        coinValueTxt.text = currentCoins.ToString() + "/3"; 
+    }
+}
