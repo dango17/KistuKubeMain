22982b238d0b1f6ddd103daca8f8f89fe51e3914
dango17 <danieloldham75@gmail.com>
2021-03-12 00:18:24 +0000

    Portals start

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject TeleportDestination;
+   // public GameObject TeleportStart; 
+
+    public void OnTriggerEnter(Collider collision)
+    { 
+        if(collision.gameObject.CompareTag("Teleporter 1"))
+        {
+            Player.transform.position = TeleportDestination.transform.position;
+        }       
+       // if(collision.gameObject.CompareTag("Teleporter 2"))
+     //   {
+       //     Player.transform.position = TeleportStart.transform.position; 
+     //   }
+    } 
+}
