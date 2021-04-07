0f94575e1a1918a8b7cb2e18d796647bab404521
dango17 <danieloldham75@gmail.com>
2021-04-07 18:12:49 +0100

    Level 3 (Full Gameloop) :100:

---
+++ Assets/Scripts/FinGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinGame : MonoBehaviour
+{
+    public void NextLevel()
+    {
+        Debug.Log("Level Complete");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
